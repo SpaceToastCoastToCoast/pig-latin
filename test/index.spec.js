7b2097e346ce1ba8ea5f3bc89c3ecf763a6d5913
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-19 19:55:34 -1000

    adding test case for vowel English words

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,7 +4,13 @@ const pigLatinizer = require('../index.js');
 const expect = chai.expect;
 
 describe('english to pig latin', function() {
+
+
   it('should translate words beginning with consonants', function() {
-    expect(pigLatinizer('banana', 'to Pig Latin')).to.deep.equal('anana-bay');
+    expect(pigLatinizer('banana', 'to')).to.deep.equal('anana-bay');
+  });
+
+  it('should translate words beginning with vowels', function() {
+    expect(pigLatinizer('omelet', 'to')).to.deep.equal('omeletay');
   });
 });
