ed08cb17d8bc747e1dd59491bd8efa4636e00fc9
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-19 20:35:19 -1000

    passed first 3 tests, single words translate properly into pig latin

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 pigLatinizer = function(input, toOrFrom) {
   var str;
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+
   if(typeof input !== 'string' || typeof toOrFrom !== 'string') {
     throw new TypeError('first parameter must be the word to translate and second parameter must be `to` or `from` pig Latin');
   }
+
+  if(toOrFrom === 'to') {
+    if(vowels.indexOf(input.charAt(0)) > -1) {
+      str = input += 'ay';
+      return str;
+    } else {
+      var startChar = input.charAt(0).toLowerCase();
+      str = input.slice(1);
+      str += ('-' + startChar + 'ay');
+      return str;
+    }
+  }
+
+  return str;
 };
 
 module.exports = pigLatinizer;
